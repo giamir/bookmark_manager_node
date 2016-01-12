09580596d81b24122c96a01f9f0945d0eac61d0d
michaellennox <michaelctlennox@gmail.com>
2016-01-12 16:49:07 +0000

    Removes console.logs used for debug

--- routes/links.js
+++ routes/links.js
@@ -6,17 +6,13 @@ var Tag = require('../models/tag');
 
 router.get('/', function(req, res) {
   Link.find().populate('tags').exec(function(err, links) {
-    // console.log(links);
     res.render('links/index', { links: links });
   });
 });
 
 router.post('/', function(req, res) {
   var newTag = new Tag({name: req.body.tags});
-  console.log(newTag);
-  console.log('thing');
   var newLink = new Link({name: req.body.name, url: req.body.url, tags: newTag.id});
-  console.log(newLink);
   newLink.save();
   newTag.save();
   res.redirect('/links');
